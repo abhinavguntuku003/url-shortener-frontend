2ef6bd3b3c6f994ac059b523a29e3f4b2810d932
abhinavguntuku003 <abhinavguntuku21@gmail.com>
2026-01-30 01:10:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import { QRCodeCanvas } from "qrcode.react";
+
 
 function App() {
+  const [longUrl, setLongUrl] = useState("");
+  const [shortUrl, setShortUrl] = useState("");
+
+  const shortenUrl = async () => {
+    const res = await axios.post("https://url-shortener-abhinav.onrender.com/api/shorten", {
+      long_url: longUrl,
+    });
+    setShortUrl(res.data.short_url);
+  };
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(shortUrl);
+    alert("Copied to clipboard!");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles.container}>
+      <h2>🔗 React URL Shortener</h2>
+
+      <input
+        type="text"
+        placeholder="Enter Long URL"
+        value={longUrl}
+        onChange={(e) => setLongUrl(e.target.value)}
+        style={styles.input}
+      />
+
+      <button onClick={shortenUrl} style={styles.button}>
+        Shorten
+      </button>
+
+      {shortUrl && (
+        <div style={styles.result}>
+          <p>Short URL:</p>
+          <a href={shortUrl} target="_blank" rel="noreferrer">
+            {shortUrl}
+          </a>
+
+          <button onClick={copyToClipboard} style={styles.copyBtn}>
+            📋 Copy
+          </button>
+
+          <h3>QR Code</h3>
+          <QRCodeCanvas value={shortUrl} size={150} />
+        </div>
+      )}
     </div>
   );
 }
+
+const styles = {
+  container: {
+    textAlign: "center",
+    marginTop: "100px",
+    fontFamily: "Arial",
+  },
+  input: {
+    padding: "10px",
+    width: "300px",
+  },
+  button: {
+    padding: "10px 20px",
+    marginLeft: "10px",
+    background: "#ff0080",
+    color: "white",
+    border: "none",
+  },
+  copyBtn: {
+    marginLeft: "10px",
+    padding: "5px 10px",
+    background: "#00bfff",
+    border: "none",
+    color: "white",
+  },
+  result: {
+    marginTop: "20px",
+  },
+};
 
 export default App;
